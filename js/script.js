8b299ea7c85a84d2b29a363a3f9dba98f0346a52
Bob Ingram <bob@theparamountgroup.us>
2016-01-19 07:20:10 -0500

    Update Please
    I am begging you to update

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ function modify_qty(val) {
 	
     return new_qty;
 }
+
+var ViewModel = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable('KITKAT');
+	this.imgSrc = ko.observable('img/kitkat-opt-230x153.jpg');
+	this.imgAttribution = ko.observable('http://www.theparamountgroup.us');
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+//	console.log(this.name);
+//	console.dir(this.name);
+
+	};
+};
+	
+ko.applyBindings(new ViewModel());
