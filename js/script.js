5a619e163331e05dfaa83579e1be1c61ba952c9d
Bob Ingram <bob@theparamountgroup.us>
2016-01-20 11:34:19 -0500

    first draft using knockout
    knockout for only one cat

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // JavaScript Document - Counter Code discovered on Code Academy exercise and modified for use
-function modify_qty(val) {
+function modify_qty(val) {   // This is the original code I used before we got to knockout.
     var qty = document.getElementById('qty').value;
     var new_qty = parseInt(qty,10) + val;
     
@@ -10,8 +10,8 @@ function modify_qty(val) {
 
 var ViewModel = function() {
 	this.clickCount = ko.observable(0);
-	this.name = ko.observable('KITKAT');
-	this.imgSrc = ko.observable('img/kitkat-opt-230x153.jpg');
+	this.name = ko.observable('Kitkat');
+	this.imgSrc = ko.observable('img/kitkat-opt-1920x1280.jpg');
 	this.imgAttribution = ko.observable('http://www.theparamountgroup.us');
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
